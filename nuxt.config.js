02af781af61d0263f553200c199175116cc302fc
Muhammad Ikhsan Morgan Nurfiantoro <150980349+M0R6@users.noreply.github.com>
2024-07-14 13:54:53 +0700

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - morganroomrecords',
-    title: 'morganroomrecords',
+    titleTemplate: '',
+    title: 'Morgan Room Records',
     htmlAttrs: {
       lang: 'en'
     },
